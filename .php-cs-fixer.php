21dd1dcc28328e18a01687b375923d8981e45da4
Andreas Möller <am@localheinz.com>
2025-08-30 18:17:03 +0200

    Fix: Use trailing_comma_in_multiline instead of trailing_comma_in_multiline_array fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -206,7 +206,11 @@ return $config
         'single_quote' => true,
         'standardize_not_equals' => true,
         'ternary_to_null_coalescing' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => [
+            'elements' => [
+                'arrays',
+            ],
+        ],
         'trim_array_spaces' => true,
         'unary_operator_spaces' => true,
         'visibility_required' => [
